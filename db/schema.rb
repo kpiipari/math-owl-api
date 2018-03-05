f2d0b52eec75d7cef9d9f00ad9b2c2074221934b
github username <github email address>
2018-03-05 08:49:08 +0000

    Creates addition_game table and migrates database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304230506) do
+ActiveRecord::Schema.define(version: 20180305082204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addition_games", force: :cascade do |t|
+    t.jsonb    "rounds"
+    t.integer  "score"
+    t.time     "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string   "name"
