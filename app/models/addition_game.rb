c22f3467df3f3e749dc95862fd497b82c3b36082
github username <github email address>
2018-03-06 22:26:19 +0000

    Adds create_round and create_rounds methods to the AdditionGame model to generate rounds json

--- app/models/addition_game.rb
+++ app/models/addition_game.rb
@@ -1,4 +1,5 @@
 class AdditionGame < ApplicationRecord
+    require 'json'
 
     def create_round
         num1 = rand(0...10)
@@ -12,11 +13,14 @@ class AdditionGame < ApplicationRecord
         rounds = {}
         i = 0
 
-        while i >10
-
+        while i < 10
+            key = "round" + i.to_s
+            value = create_round
+            rounds[:"#{key}"] = value
+            i = i + 1
         end
         
-        return rounds
+        return rounds.to_json
     end
 
 end
