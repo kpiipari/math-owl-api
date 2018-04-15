4a54c778634ab0b518ba6522d04e3e2849f10e51
github username <github email address>
2018-04-15 22:58:26 +0100

    Adds create, show, update and set_game and addition_game_params private methods to AdditionGame controller

--- app/controllers/api/addition_game_controller.rb
+++ app/controllers/api/addition_game_controller.rb
@@ -1,7 +1,43 @@
 class Api::AdditionGameController < ApplicationController
 
+    before_action :set_game, only: [:show, :edit]
+
     def index
         render json: AdditionGame.all 
+    end
+
+    def create
+        game = AdditionGame.new
+        game.rounds = game.create_round
+        game.score = 0
+        game.time = Time.now
+        if game.save
+            render json: game 
+        else
+            render json: {message: game.errors }, status: 400
+        end  
+    end
+
+    def show
+        render json: @game
+    end
+
+    def update
+        if @game.update(addition_game_params)
+            render json: @game
+        else
+            render json: { message: @game.errors }, status: 400
+        end
+    end
+
+    private
+
+    def set_game
+        @game = AdditionGame.find_by(id: params[:id])
+    end
+
+    def addition_game_params
+        params.require(:addition_game).permit(:rounds, :score, :time)
     end
 
 end
