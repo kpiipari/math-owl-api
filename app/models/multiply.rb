9ac6e0bb5cfb14672689edbea319e8f86acbb8a1
github username <github email address>
2018-04-26 21:37:38 +0100

    Adds create_round methods for substraction and multiply models

--- app/models/multiply.rb
+++ app/models/multiply.rb
@@ -1,2 +1,12 @@
 class Multiply < ApplicationRecord
+    require 'json'
+
+    def create_round
+        num1 = rand(0...10)
+        num2 = rand(0...10)
+        correct_value = num1 * num2
+
+        round = {num1: num1, num2: num2, correct_value: correct_value}
+        return round.to_json
+    end
 end
