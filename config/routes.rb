4106b169a4dc1cb89fd2822639e9497eb18c6b13
github username <github email address>
2018-04-26 21:34:48 +0100

    Adds routes for substraction and multiply

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :addition, except: [:new, :edit]
+    resources :substraction, except: [:new, :edit]
+    resources :multiply, except: [:new, :edit]
   end
 end
