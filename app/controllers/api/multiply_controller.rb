da7a357130cc8897bf422d12f8597971820c99da
github username <github email address>
2018-04-27 00:15:55 +0100

    Updates substraction and multiply controllers and models

--- app/controllers/api/multiply_controller.rb
+++ app/controllers/api/multiply_controller.rb
@@ -1,2 +1,43 @@
 class Api::MultiplyController < ApplicationController
+    before_action :set_game, only: [:show, :edit]
+
+    def index
+        render json: Multiply.all 
+    end
+
+    def create
+        game = Multiply.new
+        game.score = 0
+        game.time = Time.now
+        game.rounds = game.generate_rounds
+      
+        if game.save
+            render json: game 
+        else
+            render json: {message: game.errors }, status: 400
+        end
+    end
+
+    def show
+        render json: @game
+    end
+
+    def update
+        if @game.update(multiply_params)
+            render json: @game
+        else
+            render json: { message: @game.errors }, status: 400
+        end
+    end
+
+    private
+
+    def set_game
+        @game = Multiply.find_by(id: params[:id])
+    end
+
+    def multiply_params
+        params.require(:multiply_game).permit(:rounds, :score, :time)
+    end
+
 end
--- app/controllers/api/substraction_controller.rb
+++ app/controllers/api/substraction_controller.rb
@@ -1,2 +1,43 @@
 class Api::SubstractionController < ApplicationController
+    before_action :set_game, only: [:show, :edit]
+
+    def index
+        render json: Substraction.all 
+    end
+
+    def create
+        game = Substraction.new
+        game.score = 0
+        game.time = Time.now
+        game.rounds = game.generate_rounds
+      
+        if game.save
+            render json: game 
+        else
+            render json: {message: game.errors }, status: 400
+        end
+    end
+
+    def show
+        render json: @game
+    end
+
+    def update
+        if @game.update(substraction_params)
+            render json: @game
+        else
+            render json: { message: @game.errors }, status: 400
+        end
+    end
+
+    private
+
+    def set_game
+        @game = Substraction.find_by(id: params[:id])
+    end
+
+    def substraction_params
+        params.require(:substraction_game).permit(:rounds, :score, :time)
+    end
+
 end
