9f6a7f704fb34d639bc584e2d812f3c809b3fc1b
github username <github email address>
2018-05-15 10:41:40 +0100

    Adds methods to calculate total score for each game model and a total_score method to add scores from all games together

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,23 @@ class Player < ApplicationRecord
     has_many :substractions
     has_many :multiplies
 
+    def addition_game_scores(player)
+        games = player.additions
+        return games.reduce(0) { |sum, game| sum + game.score}
+    end
+
+    def subtract_game_scores(player)
+        games = player.substractions
+        return games.reduce(0) { |sum, game| sum + game.score}
+    end
+
+    def multiply_game_scores(player)
+        games = player.multiply
+        return games.reduce(0) { |sum, game| sum + game.score}
+    end
+
+    def total_score(player)
+        return addition_game_scores(player) + subtract_game_scores(player) + multiply_game_scores(player)
+    end
+
 end
