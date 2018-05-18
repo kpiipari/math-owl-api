f75b81707965a538b97c0ef5526317c4b94f4c9c
github username <github email address>
2018-05-18 21:36:31 +0100

    Adds player_id attribut to SubtractionSerializer. Fixes a typo in PlayerSerializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,7 +1,7 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :rank, :total_score
   has_many :additions
-  has_many :substractions
+  has_many :subtractions
   has_many :multiplies
 end
 
