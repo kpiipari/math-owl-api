26826ec4808d5df7ff951fd471cc43d2e2f5c65d
github username <github email address>
2018-05-23 10:29:52 +0100

    More CORS fixes

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Myapp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
       end
     end
   end
