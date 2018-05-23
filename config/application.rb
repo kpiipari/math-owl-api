1a43b65537e1df25cc500136e682df89d215a289
github username <github email address>
2018-05-23 09:41:40 +0100

    Updates application.rb in config

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,12 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.api_only = true
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
+      end
+    end
   end
 end
